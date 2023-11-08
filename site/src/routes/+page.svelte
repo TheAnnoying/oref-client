71b201b030ebc2873cbd23ef3b77b867210d04d0
TheAnnoying <drork2018@gmail.com>
2023-11-08 23:02:25 +0200

    added more handling for errors

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -2,10 +2,11 @@
 	import socket from "$lib/index.js";
 	import * as Alert from "$lib/components/ui/alert";
 	import * as Table from "$lib/components/ui/table";
+	import { fly } from "svelte/transition";
 
 	let history, users, alert, error;
 	socket.on("users", msg => users = msg);
-	socket.on("alert", msg => alert = msg);
+	socket.on("alert", msg => { alert = msg; error = false; });
 	socket.on("history", msg => {
 		let temp = [...msg];
 		temp.forEach((element, i) => {
@@ -15,16 +16,18 @@
 			};
 		});
 
+		error = false;
 		history = temp;
 	});
 
+	socket.on("error", () => error = true);
 	socket.io.on("error", () => error = true);
 	socket.io.on("reconnect", () => error = false);
 </script>
 
 {#if error}
-	<div class="absolute top-5 m-10">
-		<Alert.Root variant="destructive">
+	<div class="fixed bottom-0 max-w-full mt-20" transition:fly>
+		<Alert.Root variant="destructive" class="rounded-b-none border-b-0 bg-background">
 			<Alert.Title>שגיאה!</Alert.Title>
 			<Alert.Description>החיבור לשרת שלנו לא פועל כעת, נא לא לסמוך על המידע שרשום באתר.<br>השתמשו באתר של <a href="https://oref.org.il/" class="underline" target="_blank">פיקוד העורף</a> לבינתיים.</Alert.Description>
 		</Alert.Root>
@@ -59,7 +62,7 @@
 					<Table.Row>
 						<Table.Cell>{data.title}</Table.Cell>
 						<Table.Cell>{data.data}</Table.Cell>
-						<Table.Cell>{data.alertDate}</Table.Cell>
+						<Table.Cell>{data.alertDate.toLocaleString()}</Table.Cell>
 					</Table.Row>
 				{/each}
 			</Table.Body>
