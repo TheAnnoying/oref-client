9cd2bd26f5c9781fe0ec67e58e1ae42b32722b37
TheAnnoying <drork2018@gmail.com>
2024-01-18 22:08:01 +0200

    feat: added logging and less data handling on the client side

--- site/src/lib/index.js
+++ site/src/lib/index.js
@@ -1,30 +1,32 @@
 import { io } from "socket.io-client";
+import { log } from "./components/util/log.js";
 import { writable } from "svelte/store";
 const socket = io("http://localhost:3000");
 
 export let
-    history = writable(null),
-    alert = writable(null),
-    error = writable(false),
-    areas = writable(null),
-    users = writable(0);
+    history = writable([]),
+    alert = writable({}),
+    connected = writable(false),
+    districts = writable(null),
+    usercount = writable(0);
 
-socket.on("users", msg => users.set(msg));
-socket.on("areas", msg => areas.set(msg));
-socket.on("alert", msg => { alert.set(msg); error.set(false); });
-socket.on("history", msg => {
-    let temp = [...msg];
-    temp.forEach((element, i) => {
-        if(temp[i+1]?.alertDate === element.alertDate && temp[i + 1]?.title === element.title) {
-            element.data += `, ${temp[i + 1].data}`;
-            temp.splice(i + 1, 1);
-        };
-    });
+socket.on("connect", () => log("Connected to WS"));
 
-    error.set(false);
-    history.set(temp);
+socket.on("usercount", msg => usercount.set(msg));
+socket.on("districts", msg => districts.set(msg));
+socket.on("alert", msg => alert.set(msg));
+socket.on("history", msg => history.set(msg));
+
+const setConnected = (bool = false) => connected.set(bool);
+
+socket.io.on("reconnect_failed", setConnected);
+socket.io.on("connect_error", setConnected);
+socket.io.on("error", setConnected);
+socket.on("disconnect", setConnected);
+socket.on("connect", () => setConnected(true));
+
+socket.io.on("reconnect", () => log("Reconnected to WS"));
+socket.io.on("reconnect_attempt", () => {
+    log("Attempting a WS reconnect...", "warn");
+    setConnected();
 });
-
-socket.on("error", () => error.set(true));
-socket.io.on("error", () => error.set(true));
-socket.io.on("reconnect", () => error.set(false));
