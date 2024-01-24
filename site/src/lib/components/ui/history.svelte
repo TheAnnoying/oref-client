6c9a3d69bd2332b7752bb071b94f68d22c0c3333
TheAnnoying <drork2018@gmail.com>
2024-01-24 13:51:51 +0200

    feat: component supports history ranges (day, week, month)

--- site/src/lib/components/ui/history.svelte
+++ site/src/lib/components/ui/history.svelte
@@ -1,34 +1,45 @@
 <script>
-	import { relativeDate } from "../util/relativeDate.js";
+	export let type;
     import { history } from "$lib/index.js";
+
+	import * as Table from "$lib/components/ui/table";
+	import * as Tooltip from "$lib/components/ui/tooltip";
+
 	import { Skeleton } from "$lib/components/ui/skeleton";
-    import * as Table from "$lib/components/ui/table";
+	import { relativeDate } from "../util/relativeDate.js";
     import { fade } from "svelte/transition";
 </script>
-{#if $history.length > 0}
-	<div class="mt-10" transition:fade={{ duration: 350 }}>
+{#if $history[type]?.length > 0}
+	<div transition:fade={{ duration: 350 }} class="w-[600px]">
 		<Table.Root>
 			<Table.Caption>היסטורית ההתרעות</Table.Caption>
 			<Table.Header>
 				<Table.Head class="text-right">סוג מתפקה</Table.Head>
 				<Table.Head class="text-right">מיקום</Table.Head>
-				<Table.Head>זמן</Table.Head>
+				<Table.Head class="text-left">זמן</Table.Head>
 			</Table.Header>
 			<Table.Body>
-				{#each $history as data, i (i)}
+				{#each $history[type] as data, i (i)}
 					<Table.Row>
-						<Table.Cell>{data.type}</Table.Cell>
-						<Table.Cell>{data.location}</Table.Cell>
-						<Table.Cell>{relativeDate(data.date)}</Table.Cell>
+						<Table.Cell class="text-right">{data.type}</Table.Cell>
+						<Table.Cell class="text-right">{data.location}</Table.Cell>
+						<Table.Cell class="text-left">
+							<Tooltip.Root>
+								<Tooltip.Trigger>{relativeDate(data.date, data.time)}</Tooltip.Trigger>
+								<Tooltip.Content><p>{relativeDate(data.date, data.time, true)}</p></Tooltip.Content>
+							</Tooltip.Root>
+						</Table.Cell>
 					</Table.Row>
 				{/each}
 			</Table.Body>
 		</Table.Root>
 	</div>
+{:else if $history[type]?.length === 0}
+	<p class="text-2xl font-semibold m-5">אין התרעות.</p>
 {:else}
-	<div class="mt-10 pointer-events-none">
+	<div class="pointer-events-none">
 		<Table.Root>
-			<Table.Caption><Skeleton class="w-[500px] h-[20px]" /></Table.Caption>
+			<Table.Caption><Skeleton class="w-[600px] h-[20px]" /></Table.Caption>
 			<Table.Header>
 				<Table.Head><Skeleton class="w-full h-[30px]" /></Table.Head>
 				<Table.Head><Skeleton class="w-full h-[30px]" /></Table.Head>
