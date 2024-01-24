e5a1a1509393e847f05927bb8405a88c498dae05
TheAnnoying <drork2018@gmail.com>
2024-01-24 13:53:55 +0200

    feat: splits dates according to data sent by the new endpoint for history

--- site/src/lib/components/util/relativeDate.js
+++ site/src/lib/components/util/relativeDate.js
@@ -1,17 +1,14 @@
-export function relativeDate(dateStr) {
-    const [ date, time ] = dateStr.split(" ");
-    const [ hours, minutes ] = time.split(":");
-    const [ year, month, day ] = date.split("-");
+export function relativeDate(date, time, fullDate = false) {
+	const [ hours, minutes ] = time.split(":");
+	const [ day, month, year ] = date.split(".");
+	
+	if(fullDate) return `${day}/${month}/${year} - ${hours}:${minutes}`;
+	
+	let now = new Date();
+	let inputDate = new Date(year, month - 1, day, hours, minutes);
+	let diffInDays = Math.floor((now - inputDate) / (1000 * 60 * 60 * 24));
 
-    let now = new Date();
-    let inputDate = new Date(year, month - 1, day, hours, minutes);
-
-    let diffInDays = Math.floor((now - inputDate) / (1000 * 60 * 60 * 24));
-    if (diffInDays === 0) {
-        return `${hours}:${minutes}`;
-    } else if (diffInDays === 1) {
-        return `אתמול ב-${hours}:${minutes}`;
-    } else {
-        return `${day}/${month}/${year} - ${hours}:${minutes}`;
-    }
+	if (diffInDays === 0) return `${hours}:${minutes}`;
+	else if (diffInDays === 1) return `אתמול ב-${hours}:${minutes}`;
+	else return `${day}.${month}.${year}`;
 }
