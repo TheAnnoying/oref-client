2119e9e4a60b988ccf9f708cde34e85d4905218c
TheAnnoying <drork2018@gmail.com>
2024-02-09 00:02:13 +0200

    chore: added nprogress, separated navbar into its own component, changed loading animation

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,69 +1,43 @@
 <script>
 	import "../app.css";
-	import { districts, usercount, connected } from "$lib/index.js";
+	import "nprogress/nprogress.css";
+	import Navbar from "$lib/components/ui/navbar.svelte";
 
-	import { Button } from "$lib/components/ui/button";
-	import { Badge } from "$lib/components/ui/badge";
-	import { Skeleton } from "$lib/components/ui/skeleton";
-	import { Sun, Moon, Settings2, RefreshCcw } from "lucide-svelte";
-	import { toggleMode } from "mode-watcher";
+	import nProgress from "nprogress";
+	import { browser } from "$app/environment";
+	import { navigating } from "$app/stores";
+
+	import { connected, connectWebsocket } from "$lib/index.js";
+	import { Loader2, Info } from "lucide-svelte";
 	import { fade } from "svelte/transition";
+	import { ModeWatcher } from "mode-watcher";
 
-	import * as Dialog from "$lib/components/ui/dialog";
-	import * as Command from "$lib/components/ui/command";
+	connectWebsocket();
+	nProgress.configure({
+		minimum: 0.4,
+		trickleSpeed: 200,
+		showSpinner: false,
+		easing: "ease-out",
+		speed: 300
+	});
 
-	let value = "", settings;
-
-	$: {
-		value = $districts?.find(f => f.value === value)?.label ?? "בחר אזורי התרעות";
-	}
+	$: if(browser) $navigating ? nProgress.start() : nProgress.done();
 </script>
+
 <svelte:head>
 	<title>קליינט פיקוד העורף</title>
 </svelte:head>
 
+<ModeWatcher />
+
 {#if $connected === true}
-	<div class="flex items-center backdrop-blur-md p-4 fixed top-0 w-full border-b z-10" transition:fade={{ delay: 300, duration: 200 }}>
-		<div class="flex flex-[1]">
-			<Badge variant="outline">משתמשים: {$usercount ?? "0"}</Badge>
-		</div>
-		<p class="text-xl lowercase">Oref Client</p>
-		<div class="flex flex-[1] justify-end gap-1">
-			<Button variant="outline" size="icon" on:click={() => { settings = true }}><Settings2 /></Button>
-			<Button on:click={toggleMode} variant="outline" size="icon">
-				<Sun class="dark:hidden" />
-				<Moon class="hidden dark:block" />
-			</Button>
-		</div>
+	<div class="contents">
+		<Navbar />
+		<a class="md:block hidden fixed bottom-5 right-5" href="https://www.oref.org.il/12761-he/Pakar.aspx" target="_blank"><Info /></a>
+		<slot />
 	</div>
-	<Dialog.Root bind:open={settings}>
-		<Dialog.Trigger />
-		<Dialog.Content>
-			<Dialog.Header>
-				<Dialog.Title class="text-right flex items-baseline gap-2">הגדרות</Dialog.Title>
-				<Dialog.Description class="text-right">כאן תוכלו להגדיר את הצורה בה האתר מתנהג</Dialog.Description>
-			</Dialog.Header>
-			{#if $districts}
-				<Command.Root class="border h-auto">
-					<Command.Input placeholder="התחל להקליד בשביל להראות תוצאות" />
-					<Command.List>
-						<Command.Empty>לא נמצאו תוצאות</Command.Empty>
-						<Command.Group heading="אזורים">
-							{#each $districts as area}
-								<Command.Item value={area.label} onSelect={(current => { value = current; open = false })}>{area.label}</Command.Item>
-							{/each}
-						</Command.Group>
-					</Command.List>
-				</Command.Root>
-			{:else}
-				<Skeleton class="rounded-full"></Skeleton>
-			{/if}
-		</Dialog.Content>
-	</Dialog.Root>
-	<div transition:fade={{ delay: 300, duration: 200 }}><slot/></div>
 {:else}
 	<div class="fixed bg-background w-screen h-screen flex flex-row gap-5 justify-center items-center z-20" transition:fade={{ duration: 75 }}>
-		<RefreshCcw class="animate-spin" />
-		<p>מתחבר...</p>
+		<Loader2 class="animate-spin h-11 w-11" />
 	</div>
 {/if}
