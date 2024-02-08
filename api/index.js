bb3e6a78e1dd512cbf17d4dbbb58e2a0cc994354
TheAnnoying <drork2018@gmail.com>
2024-02-08 23:53:35 +0200

    feat: removed different history types, changed districts to cities

--- api/index.js
+++ api/index.js
@@ -8,24 +8,23 @@ const io = new Server(server, {
 });
 
 const fetchURLS = {
-	"history_day": "https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&mode=1",
-	"history_week": "https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&mode=2",
-	"history_month": "https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&mode=3",
+	"history": "https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&fromDate=01.01.2005&toDate=&mode=0",
 	"alert": "https://www.oref.org.il/WarningMessages/alert/alerts.json",
-	"districts": "https://www.oref.org.il/Shared/Ajax/GetDistricts.aspx?lang=he",
+	"cities": "https://www.oref.org.il/Shared/Ajax/GetCitiesMix.aspx?lang=he",
 	"instructions": "https://www.oref.org.il/Shared/Ajax/GetAlarmInstructions.aspx?lang=he"
 }
 
 async function fetchData(type = "alert") {
-	if (["history_day", "history_week", "history_month"].includes(type.toLowerCase())) {
+	if(type.toLowerCase() === "history") {
 		let history = [];
 		const res = await fetch(fetchURLS[type.toLowerCase()]).then(e => e.json()).catch(() => {});
 		if(res) {
 			res.forEach((e, i) => {
-				const lastElement = res[i - 1];
 				let location = e.data;
 
-				if (e.alertDate === lastElement?.alertDate && e.title === lastElement?.title) location += `, ${lastElement.data}`;
+				const existing = history.find(v => v.time.slice(0, 5) === e.time.slice(0, 5) && v.date === e.date && v.type === e["category_desc"]);
+				if(existing) return existing.location += `, ${location}`;
+
 				history.push({
 					date: e.date,
 					time: e.time,
@@ -36,7 +35,7 @@ async function fetchData(type = "alert") {
 		}
 
 		return history;
-	} else if (type.toLowerCase() === "alert") {
+	} else if(type.toLowerCase() === "alert") {
 		let alert = {};
 		const res = await fetch(fetchURLS[type.toLowerCase()], {
 			headers: {
@@ -55,6 +54,12 @@ async function fetchData(type = "alert") {
 		}
 
 		return alert;
+	} else if(type.toLowerCase() === "cities") {
+		let cities = [];
+		const res = await fetch(fetchURLS[type.toLowerCase()]).then(e => e.json()).catch(() => {});
+		if(res) res.forEach(e => cities.push(e.label));
+
+		return cities;
 	} else {
 		const res = await fetch(fetchURLS[type.toLowerCase()]).then(e => e.json()).catch(() => { });
 		return res;
@@ -69,7 +74,7 @@ function emit(name, value) {
 
 async function sendData() {
 	try {
-		await emit("usercount", io.sockets.sockets.size - 1);
+		await emit("usercount", io.engine.clientsCount);
 
 		const alert = await fetchData("alert");
 		const keyAmount = Object.keys(alert).length;
@@ -80,11 +85,7 @@ async function sendData() {
 			await emit("alert", alert);
 			await emit("instructions", await fetchData("instructions"));
 
-			await emit("history", {
-				day: await fetchData("history_day"),
-				week: await fetchData("history_week"),
-				month: await fetchData("history_month")
-			});
+			await emit("history", await fetchData("history"));
 		} else if (keyAmount === 0 && alertRecently) {
 			alertRecently = false;
 			await emit("alert", {});
@@ -101,13 +102,9 @@ sendData();
 io.on("connection", async socket => {
 	console.log("a user connected");
 
-	socket.emit("history", {
-		day: await fetchData("history_day"),
-		week: await fetchData("history_week"),
-		month: await fetchData("history_month")
-	});
+	socket.emit("history", await fetchData("history"));
 
-	socket.emit("districts", await fetchData("districts"));
+	socket.emit("cities", await fetchData("cities"));
 	socket.on("disconnect", () => console.log("a user disconnected"));
 });
 
