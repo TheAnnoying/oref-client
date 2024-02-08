cc1b6ac381ed6c9ce597dda8c4d9537c73266b78
TheAnnoying <drork2018@gmail.com>
2024-02-09 00:01:13 +0200

    feat: created a settings page

---
+++ site/src/routes/settings/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import LocationChooser from "$lib/components/ui/settings/location-chooser.svelte";
+	import Alert from "$lib/components/ui/alert.svelte";
+    import { Checkbox } from "$lib/components/ui/checkbox";
+    import { fade } from "svelte/transition";
+
+    let checked;
+</script>
+<div class="flex flex-col items-center mt-28 mb-10">
+    <div class="flex items-center gap-3 p-3">
+        <Checkbox id="only_show_selected_location_alerts_in_history" bind:checked />
+        <p>ההיסטוריה תראה רק התרעות מהמיקומים המוגדרים</p>
+    </div>
+    <LocationChooser />
+</div>
+<div class="fixed bottom-5" transition:fade={{ duration: 350 }}>
+    <Alert />
+</div>
