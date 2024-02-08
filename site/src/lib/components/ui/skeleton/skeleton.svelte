1845d5e42abf88b0f6ffb9b4512656112c9e5476
TheAnnoying <drork2018@gmail.com>
2024-02-09 00:03:27 +0200

    style: created a different animation for the skeleton

--- site/src/lib/components/ui/skeleton/skeleton.svelte
+++ site/src/lib/components/ui/skeleton/skeleton.svelte
@@ -7,5 +7,21 @@
 	let className: $$Props["class"] = undefined;
 	export { className as class };
 </script>
+<div class={cn("skeleton", className)} {...$$restProps} />
+<style>
+	.skeleton {
+		will-change: background-position;
+		animation: skeleton 1.8s ease-in-out infinite;
+		background-image: linear-gradient(105deg, transparent 0%, transparent 40%, hsl(var(--secondary-foreground)) 50%, transparent 60%, transparent 100%);
+		@apply bg-muted rounded-md bg-[length:200%];
+	}
 
-<div class={cn("animate-pulse rounded-md bg-muted", className)} {...$$restProps} />
+	@keyframes skeleton {
+		0% {
+			background-position: 150%;
+		}
+		100% {
+			background-position: -50%;
+		}
+	}
+</style>
