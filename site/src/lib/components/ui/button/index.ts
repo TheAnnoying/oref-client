bc9108b3b7e3f15dd72c87685fe1dc9a7905b796
TheAnnoying <drork2018@gmail.com>
2024-02-12 22:51:46 +0200

    style: created icon variant for icons used as buttons

--- site/src/lib/components/ui/button/index.ts
+++ site/src/lib/components/ui/button/index.ts
@@ -12,6 +12,7 @@ const buttonVariants = tv({
 				"border border-input bg-background hover:bg-accent hover:text-accent-foreground",
 			secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
 			ghost: "hover:bg-accent hover:text-accent-foreground",
+			icon: "!bg-transparent !hover:text-foreground !p-0",
 			link: "text-primary underline-offset-4 hover:underline"
 		},
 		size: {
