4ad2719cf02b4b9b9c78345cb12efd167f8c0e49
TheAnnoying <drork2018@gmail.com>
2024-02-14 00:12:39 +0200

    chore: removed unused options and added the edit mode switch

--- site/src/routes/settings/+page.svelte
+++ site/src/routes/settings/+page.svelte
@@ -1,15 +1,15 @@
 <script>
     import LocationChooser from "$lib/components/ui/settings/location-chooser.svelte";
+    import EditModeSwitch from "$lib/components/ui/settings/edit-mode-switch.svelte";
 	import Alert from "$lib/components/ui/alert.svelte";
-    import { Checkbox } from "$lib/components/ui/checkbox";
+
     import { fade } from "svelte/transition";
 
     let checked;
 </script>
 <div class="flex flex-col items-center mt-28 mb-10">
     <div class="flex items-center gap-3 p-3">
-        <Checkbox id="only_show_selected_location_alerts_in_history" bind:checked />
-        <p>ההיסטוריה תראה רק התרעות מהמיקומים המוגדרים</p>
+        <EditModeSwitch />
     </div>
     <LocationChooser />
 </div>
