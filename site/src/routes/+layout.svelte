980d28db3d894968545240f2ecebe39e1797d00c
TheAnnoying <drork2018@gmail.com>
2024-02-14 00:13:16 +0200

    chore: added a skip navigation button

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import "../app.css";
 	import "nprogress/nprogress.css";
+	import { Button } from "$lib/components/ui/button";
 	import Navbar from "$lib/components/ui/navbar.svelte";
 
 	import nProgress from "nprogress";
@@ -8,7 +9,7 @@
 	import { navigating } from "$app/stores";
 
 	import { connected, connectWebsocket } from "$lib/index.js";
-	import { Loader2, Info } from "lucide-svelte";
+	import { Loader2, Info, FastForward } from "lucide-svelte";
 	import { fade } from "svelte/transition";
 	import { ModeWatcher } from "mode-watcher";
 
@@ -30,11 +31,16 @@
 
 <ModeWatcher />
 
+<Button class="fixed -top-16 focus:top-4 left-4 z-50 transition-all gap-2" href="#main">
+	Skip Navigation
+	<FastForward class="w-3 h-3" />
+</Button>
+
 {#if $connected === true}
 	<div class="contents">
 		<Navbar />
 		<a class="md:block hidden fixed bottom-5 right-5" href="https://www.oref.org.il/12761-he/Pakar.aspx" target="_blank"><Info /></a>
-		<slot />
+		<main id="main"><slot /></main>
 	</div>
 {:else}
 	<div class="fixed bg-background w-screen h-screen flex flex-row gap-5 justify-center items-center z-20" transition:fade={{ duration: 75 }}>
