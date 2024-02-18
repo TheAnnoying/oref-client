015470be63c8c14de4a43fb66da0ca1a41cf3126
TheAnnoying <drork2018@gmail.com>
2024-02-18 23:08:37 +0200

    feat: edit mode is stored in local storage

--- site/src/lib/components/ui/settings/edit-mode-switch.svelte
+++ site/src/lib/components/ui/settings/edit-mode-switch.svelte
@@ -1,9 +1,10 @@
 <script>
-    import { editMode } from "$lib/index.js";
+    import { editMode, localStorage } from "$lib/index.js";
+    editMode.subscribe(value => localStorage.set("editMode", JSON.stringify(value)));
+
     import { Switch } from "$lib/components/ui/switch";
-    let checked = $editMode;
 </script>
 <div class="flex items-center gap-2">
-    <Switch id="edit-mode" style="direction: ltr;" bind:checked onCheckedChange={() => $editMode = !checked} />
+    <Switch id="edit-mode" style="direction: ltr;" bind:checked={$editMode} />
     <p for="edit-mode">מצב עריכה</p>
 </div>
