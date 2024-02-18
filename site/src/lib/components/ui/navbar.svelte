ffb28cc5b55eac8a4af40b5df71df659a22c02f0
TheAnnoying <drork2018@gmail.com>
2024-02-18 23:08:21 +0200

    chore: removed tooltips due to accessibility concerns, now using aria labels and sr-only text

--- site/src/lib/components/ui/navbar.svelte
+++ site/src/lib/components/ui/navbar.svelte
@@ -5,31 +5,22 @@
 
     import { Button } from "$lib/components/ui/button";
     import { Badge } from "$lib/components/ui/badge";
-    import * as Tooltip from "$lib/components/ui/tooltip";
 </script>
 <div class="fixed top-0 flex items-center backdrop-blur-md p-4 w-full border-b z-10">
     <div class="flex flex-[1]">
-        <Tooltip.Root>
-            <Tooltip.Trigger>
-                <Badge variant="outline">
-                    <BarChart class="h-4 w-4 ml-1" />
-                    <p class="md:block hidden ml-1">משתמשים:</p>
-                    <p>{$usercount ?? "0"}</p>
-                </Badge>
-            </Tooltip.Trigger>
-            <Tooltip.Content>כמות האנשים המחוברים לאתר כרגע</Tooltip.Content>
-        </Tooltip.Root>
+        <Badge variant="outline">
+            <BarChart class="h-4 w-4 ml-1" />
+            <p class="md:block hidden ml-1">משתמשים:</p>
+            <p>{$usercount ?? "0"}</p>
+        </Badge>
+        <p class="sr-only">כמות האנשים המחוברים לאתר כרגע</p>
     </div>
-    <Tooltip.Root>
-        <Tooltip.Trigger><a href="/" class="text-xl lowercase select-none">Oref Client</a></Tooltip.Trigger>
-        <Tooltip.Content>עמוד הבית של האתר</Tooltip.Content>
-    </Tooltip.Root>
+    <a href="/" class="text-xl lowercase select-none" aria-label="עמוד הבית של האתר">Oref Client</a>
+    <p class="sr-only">עמוד הבית של האתר</p>
     <div class="flex flex-[1] justify-end gap-1">
-        <Tooltip.Root>
-            <Tooltip.Trigger><Button variant="outline" size="icon" href="/settings"><Settings2 /></Button></Tooltip.Trigger>
-            <Tooltip.Content>הגדרות האתר</Tooltip.Content>
-        </Tooltip.Root>
-        <Button on:click={toggleMode} variant="outline" size="icon">
+        <Button variant="outline" size="icon" href="/settings" aria-label="עמוד ההגדרות של האתר"><Settings2 /></Button>
+        <p class="sr-only">הגדרות האתר</p>
+        <Button on:click={toggleMode} variant="outline" size="icon" aria-label="כפתור לשינוי נראות האתר - בהיר או כהה">
             <Sun class="dark:hidden" />
             <Moon class="hidden dark:block" />
         </Button>
