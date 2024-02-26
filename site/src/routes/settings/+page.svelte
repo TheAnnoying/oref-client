678c89bd0763918ed944caaa92644cf7e6cd7adc
TheAnnoying <drork2018@gmail.com>
2024-02-27 00:19:06 +0200

    chore: added a proper title to the setting and removed edit mode switch that is now part of the location chooser component

--- site/src/routes/settings/+page.svelte
+++ site/src/routes/settings/+page.svelte
@@ -1,12 +1,9 @@
 <script>
     import LocationChooser from "$lib/components/ui/settings/location-chooser.svelte";
-    import EditModeSwitch from "$lib/components/ui/settings/edit-mode-switch.svelte";
 	import Alert from "$lib/components/ui/alert.svelte";
 </script>
-<div class="flex flex-col items-center mt-28 mb-10">
-    <div class="flex items-center gap-3 p-3">
-        <EditModeSwitch />
-    </div>
+<div class="flex flex-col items-center mt-28 mb-10 gap-3">
+    <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">מיקומים אשר ישמיעו התרעות קוליות:</h3>
     <LocationChooser />
 </div>
 <div class="fixed bottom-5">
