3f5f4cb6c026774ab44e1e03c33b140ee9123fb3
TheAnnoying <drork2018@gmail.com>
2024-02-27 12:13:47 +0200

    style: gave toggle a tooltip

--- site/src/lib/components/ui/settings/edit-mode-switch.svelte
+++ site/src/lib/components/ui/settings/edit-mode-switch.svelte
@@ -5,10 +5,16 @@
     editMode.subscribe(value => localStorage.set("editMode", JSON.stringify(value)));
     let className;
 
+    import * as Tooltip from "$lib/components/ui/tooltip";
     import { Pencil } from "lucide-svelte";
     import { Toggle } from "$lib/components/ui/toggle";
     export { className as class };
 </script>
-<Toggle id="edit-mode" bind:pressed={$editMode} class={cn(className)}>
-    <Pencil class="w-4 h-4" />
-</Toggle>
+<Tooltip.Root>
+    <Tooltip.Trigger>
+        <Toggle id="edit-mode" bind:pressed={$editMode} class={cn("m-1", className)} aria-label="מצב עריכה">
+            <Pencil class="w-4 h-4" />
+        </Toggle>
+    </Tooltip.Trigger>
+    <Tooltip.Content>מצב עריכה</Tooltip.Content>
+</Tooltip.Root>
