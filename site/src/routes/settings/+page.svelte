5ef026f3502d18c538159961325fb540088efadc
TheAnnoying <drork2018@gmail.com>
2024-03-11 23:55:26 +0200

    refactor: settings page uses all new components

--- site/src/routes/settings/+page.svelte
+++ site/src/routes/settings/+page.svelte
@@ -1,10 +1,15 @@
 <script>
-    import LocationChooser from "$lib/components/ui/settings/location-chooser.svelte";
+    import { fly } from "svelte/transition";
+
+    import LocationSettings from "$lib/components/ui/settings/preferred-locations/add-location.svelte";
 	import Alert from "$lib/components/ui/alert.svelte";
+    import AlertSound from "$lib/components/ui/settings/alert-sound.svelte";
 </script>
-<div class="flex flex-col items-center mt-28 mb-10 gap-3">
-    <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">מיקומים אשר ישמיעו התרעות קוליות:</h3>
-    <LocationChooser />
+<div class="flex flex-col items-center mt-28 mb-10 gap-3 max-w-7xl">
+    <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight" in:fly={{ y: -5, opacity: 0 }}>מיקומים אשר ישמיעו התרעות קוליות:</h3>
+    <LocationSettings />
+    <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight" in:fly={{ y: -5, opacity: 0 }}>קול התרעה:</h3>
+    <AlertSound />
 </div>
 <div class="fixed bottom-5">
     <Alert />
