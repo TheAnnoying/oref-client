da781b63d81c5770f2185e6aca11a21a1aaae087
TheAnnoying <drork2018@gmail.com>
2024-03-11 23:54:29 +0200

    chore: moved live alerts to be part of the history

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-	import Alert from "$lib/components/ui/alert.svelte";
 	import History from "$lib/components/ui/history.svelte";
 </script>
 
 <div class="flex flex-col items-center mt-28 mb-10">
-	<Alert />
     <History />
 </div>
