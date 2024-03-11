b6bb83b7ba4b640dbcbdd3883695301c480bf542
TheAnnoying <drork2018@gmail.com>
2024-03-11 23:47:57 +0200

    feat: page title is now shown in the navbar

--- site/src/lib/components/ui/navbar.svelte
+++ site/src/lib/components/ui/navbar.svelte
@@ -5,6 +5,9 @@
 
     import { Button } from "$lib/components/ui/button";
     import { Badge } from "$lib/components/ui/badge";
+    import { fly } from "svelte/transition";
+
+    export let title;
 </script>
 <div class="fixed top-0 flex items-center backdrop-blur-md p-4 w-full border-b z-10">
     <div class="flex flex-[1]">
@@ -15,11 +18,17 @@
         </Badge>
         <p class="sr-only">כמות האנשים המחוברים לאתר כרגע</p>
     </div>
-    <a href="/" class="text-xl lowercase select-none" aria-label="עמוד הבית של האתר">Oref Client</a>
+    <div>
+        <a href="/" class="text-xl lowercase select-none" aria-label="עמוד הבית של האתר">Oref Client</a>
+        <Badge class="grid place-items-center">
+            {#key title}
+                <span style="grid-row: 1; grid-column: 1;" in:fly={{ y: -12 }} out:fly={{ y: 12 }}>{title}</span>
+            {/key}
+        </Badge>
+    </div>
     <p class="sr-only">עמוד הבית של האתר</p>
     <div class="flex flex-[1] justify-end gap-1">
         <Button variant="outline" size="icon" href="/settings" aria-label="עמוד ההגדרות של האתר"><Settings2 /></Button>
-        <p class="sr-only">הגדרות האתר</p>
         <Button on:click={toggleMode} variant="outline" size="icon" aria-label="כפתור לשינוי נראות האתר - בהיר או כהה">
             <Sun class="dark:hidden" />
             <Moon class="hidden dark:block" />
