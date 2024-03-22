98bfd9c3afc83eb477ec673ce1b5e1b66fa651a7
TheAnnoying <drork2018@gmail.com>
2024-03-22 22:47:51 +0200

    feat!: added a map to alert pages with location markers

--- site/src/routes/alert/[id]/+page.svelte
+++ site/src/routes/alert/[id]/+page.svelte
@@ -1,14 +1,50 @@
 <script>
+	import "leaflet/dist/leaflet.css";
+	import locationList from "/src/locations.json";
 	import { history } from "$lib/index.js";
+
 	import { page } from "$app/stores";
+	import { onMount } from "svelte";
 	import { relativeDate } from "$lib/components/util/relativeDate";
 
 	import { Button } from "$lib/components/ui/button";
 	import * as Tooltip from "$lib/components/ui/tooltip";
 
 	import { fly, fade } from "svelte/transition";
-	import { Clock, HelpCircle } from "lucide-svelte";
+	import { Clock, HelpCircle, GripHorizontal } from "lucide-svelte";
 	import { alertIcons } from "$lib/utils";
+	let locationsFound = 0;
+
+	onMount(async() => {
+		const L = await import("leaflet");
+		const map = L.map("map", { zoomControl: false }).setView([32, 35], 13);
+		map.setZoom(8);
+
+		history.subscribe(() => {
+			const { location } = $history.find(e => e.id === +$page.params.id);
+
+			location.forEach(location => {
+				if(locationList[location]) {
+					locationsFound += 1;
+					const marker = L.marker([locationList[location].centerY, locationList[location].centerX]);
+					marker.bindPopup(`
+					<div style="text-align: center">
+						<p style="margin: 0;">${location}</p>
+						<p style="margin: 0;">זמן למיגון: ${locationList[location]["shelter_time"].text}</p>
+					</div>
+					`).addTo(map);
+				}
+			});
+
+			if(locationsFound !== 0) {
+				L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+					maxZoom: 13,
+					minZoom: 6,
+					attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+				}).addTo(map);
+			}
+		});
+	});
 </script>
 <div class="fixed inset-0 -z-10 bg-gradient-to-t dark:from-[#2e1212] from-[#ffe1e1] to-transparent" in:fade={{ duration: 350 }}></div>
 <div class="flex flex-col items-center mt-28 mb-10 gap-3 max-w-7xl text-center" in:fly={{ y: 5, opacity: 0, duration: 350 }}>
@@ -24,15 +60,24 @@
 					{relativeDate(data.date, data.time, true)}
 				</Tooltip.Content>
 			</Tooltip.Root>
-			<div class="flex flex-row items-center text-4xl font-semibold tracking-tight gap-7">
+			<div class="flex flex-row items-center text-4xl font-semibold tracking-tight gap-5">
 				<svelte:component this={alertIcons[data.type] ?? HelpCircle} class="h-11 w-11 text-destructive" />
 				{data.type}
 			</div>
-			{data.location}
+			{data.location.join(", ")}
+			<div id="map" in:fly={{ y: 10, opacity: 0, delay: 100 }} class="m-10 lg:w-[900px] lg:aspect-video lg:h-auto w-[350px] h-96 rounded-2xl bg-muted flex items-center justify-center border-border border-2">
+				{#if locationsFound === 0}
+					<p class="text-xl text-muted-foreground tracking-tight">לא ניתן להציג מפה למיקום המבוקש</p>
+				{:else if locationsFound !== data.location.length}
+					<p class="absolute top-[-30px] hover:top-0 transition-all text-lg text-white bg-destructive py-1 px-5 rounded-b-md tracking-tight z-[400] grid place-items-center">
+						לא היה ניתן להציג על המפה את כל המיקומים
+						<GripHorizontal />
+					</p>
+				{/if}
+			</div>
 		{:else}
 			<p class="text-4xl font-semibold tracking-tight mb-2">התרעה לא נמצאה</p>
 			<Button href="/" variant="outline">חזרה לעמוד הבית</Button>
 		{/if}
 	{/if}
-	<div id="map"></div>
 </div>
