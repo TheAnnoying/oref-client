0e1e25877aeb021f747439333ba74bffe61bab92
TheAnnoying <drork2018@gmail.com>
2024-04-13 23:05:36 +0300

    chore: created an alert tags component that returns the tags on a given alert for the alert locations

---
+++ site/src/lib/components/ui/alert-tags.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { preferredLocations } from "$lib/index.js";
+    import { Badge } from "$lib/components/ui/badge";
+    export let alertLocations;
+</script>
+{#each $preferredLocations.flatMap(e => e[0]) as cities}
+    {#each alertLocations.filter(e => cities.includes(e)) as city}
+        {#each $preferredLocations.find(e => e[0] == city)[1] as tag}
+            <Badge variant="outline" class="bg-yellow-100 dark:bg-yellow-500/20 text-yellow-700 dark:text-yellow-400 border-yellow-400 dark:border-yellow-500/30 !border-t-yellow-500/30">{tag}</Badge>
+        {/each}
+    {/each}
+{/each}
