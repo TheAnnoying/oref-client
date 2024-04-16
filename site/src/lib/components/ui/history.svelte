82f4600d960955a7ef4c2cb2325461c07a237949
TheAnnoying <drork2018@gmail.com>
2024-04-16 23:46:34 +0300

    fix: ui should update as history updates, document scrollbar does not disappear when opening the select menu

--- site/src/lib/components/ui/history.svelte
+++ site/src/lib/components/ui/history.svelte
@@ -18,103 +18,105 @@
 	let page = 0, size = 12, historyCards = [];
 	$: historyCards = [...historyCards, ...(($history ?? []).filter(e => ([null, "כל ההתרעות"].includes(localStorage.get("historyFilter")) ? e : e.type === localStorage.get("historyFilter"))).slice(size*page, size*(page+1)-1))]
 </script>
-{#if $history?.length > 0}
-	<div class="max-h-screen sm:w-[600px] w-[350px]" in:fade={{ duration: 150 }}>
-		<div class="flex flex-row items-center gap-7">
-			<Select.Root typeahead=true selected={{
-				value: localStorage.get("historyFilter") ?? "כל ההתרעות",
-				label: localStorage.get("historyFilter") ?? "כל ההתרעות",
-				disabled: false
-			}} onSelectedChange={value => {
-				localStorage.set("historyFilter", value.value);
-				page = 0, size = 12, historyCards = [];
-			}}>
-				<Select.Trigger>
-					<Select.Value placeholder="כל ההתרעות" />
-				</Select.Trigger>
-				<Select.Content fitViewport={true}>
-					<Select.Item value="כל ההתרעות" class="flex flex-row gap-2">
-						<Asterisk class="h-5 w-5 text-destructive" />
-						כל ההתרעות
-					</Select.Item>
-					{#each Object.keys(alertIcons) as type}
-						{#if $history.some(e => e.type == type)}
-							<Select.Item value={type} label={type} class="flex flex-row gap-2">
-								<svelte:component this={alertIcons[type] ?? HelpCircle} class="h-5 w-5 text-destructive" />
-								{type}
-							</Select.Item>
-						{:else}
-							<Select.Item value={type} class="flex flex-row gap-2" disabled=true>
-								<svelte:component this={alertIcons[type] ?? HelpCircle} class="h-5 w-5 text-muted-foreground" />
-								{type}
-							</Select.Item>
-						{/if}
-					{/each}
-				</Select.Content>
-			</Select.Root>
-			<Separator class="sm:w-80 w-24" />
+{#key $history}
+	{#if $history?.length > 0}
+		<div class="max-h-screen sm:w-[600px] w-[350px]" in:fade={{ duration: 150 }}>
+			<div class="flex flex-row items-center gap-7">
+				<Select.Root preventScroll={false} selected={{
+					value: localStorage.get("historyFilter") ?? "כל ההתרעות",
+					label: localStorage.get("historyFilter") ?? "כל ההתרעות",
+					disabled: false
+				}} onSelectedChange={value => {
+					localStorage.set("historyFilter", value.value);
+					page = 0, size = 12, historyCards = [];
+				}}>
+					<Select.Trigger>
+						<Select.Value placeholder="כל ההתרעות" />
+					</Select.Trigger>
+					<Select.Content fitViewport={true}>
+						<Select.Item value="כל ההתרעות" class="flex flex-row gap-2">
+							<Asterisk class="h-5 w-5 text-destructive" />
+							כל ההתרעות
+						</Select.Item>
+						{#each Object.keys(alertIcons) as type}
+							{#if $history.some(e => e.type == type)}
+								<Select.Item value={type} label={type} class="flex flex-row gap-2">
+									<svelte:component this={alertIcons[type] ?? HelpCircle} class="h-5 w-5 text-destructive" />
+									{type}
+								</Select.Item>
+							{:else}
+								<Select.Item value={type} class="flex flex-row gap-2" disabled=true>
+									<svelte:component this={alertIcons[type] ?? HelpCircle} class="h-5 w-5 text-muted-foreground" />
+									{type}
+								</Select.Item>
+							{/if}
+						{/each}
+					</Select.Content>
+				</Select.Root>
+				<Separator class="sm:w-80 w-24" />
+			</div>
+			<Alert />
+			{#each historyCards as data}
+				<a href="/alert/{data.id}">
+					<Card.Root class="my-4">
+						<Card.Header>
+							<Card.Title class="flex justify-between">
+								<div class="flex flex-row gap-2">
+									<div class="flex flex-row gap-2">
+										<svelte:component this={alertIcons[data.type] ?? HelpCircle} class="h-5 w-5 text-destructive" />
+										{data.type}
+									</div>
+									<div class="flex sm:flex-wrap flex-row gap-1 max-w-12 sm:max-w-64 overflow-auto">
+										<AlertTags alertLocations={data.location} />
+									</div>
+								</div>
+								<Tooltip.Root>
+									<Tooltip.Trigger class="flex flex-row items-center gap-2 text-sm text-muted-foreground font-normal">
+										{relativeDate(data.date, data.time)}
+										<Clock class="h-4 w-4" />
+									</Tooltip.Trigger>
+									<Tooltip.Content class="font-mono">
+										{relativeDate(data.date, data.time, true)}
+									</Tooltip.Content>
+								</Tooltip.Root>
+							</Card.Title>
+							<Card.Description>
+								{data.location.join(", ")}
+							</Card.Description>
+						</Card.Header>
+					</Card.Root>
+				</a>
+			{/each}
+			<InfiniteScroll window={true} threshold={100} on:loadMore={() => page++} />
 		</div>
-		<Alert />
-		{#each historyCards as data}
-			<a href="/alert/{data.id}">
+	{:else if $history?.length === 0}
+		<p class="text-3xl font-semibold">אין התרעות.</p>
+	{:else}
+		<div class="sm:w-[600px] w-[350px]">
+			<div class="flex flex-row items-center gap-7">
+				<Select.Root>
+					<Select.Trigger>
+						<Skeleton class="w-full h-full mx-2"/>
+					</Select.Trigger>
+				</Select.Root>
+				<Separator class="sm:w-80 w-28" />
+			</div>
+			{#each new Array(12).fill("") as element}
 				<Card.Root class="my-4">
 					<Card.Header>
 						<Card.Title class="flex justify-between">
 							<div class="flex flex-row gap-2">
-								<div class="flex flex-row gap-2">
-									<svelte:component this={alertIcons[data.type] ?? HelpCircle} class="h-5 w-5 text-destructive" />
-									{data.type}
-								</div>
-								<div class="flex sm:flex-wrap flex-row gap-1 max-w-12 sm:max-w-64 overflow-auto">
-									<AlertTags alertLocations={data.location} />
-								</div>
+								<Skeleton class="h-5 w-5 rounded-full" />
+								<Skeleton class="w-[200px] h-[20px]" />
 							</div>
-							<Tooltip.Root>
-								<Tooltip.Trigger class="flex flex-row items-center gap-2 text-sm text-muted-foreground font-normal">
-									{relativeDate(data.date, data.time)}
-									<Clock class="h-4 w-4" />
-								</Tooltip.Trigger>
-								<Tooltip.Content class="font-mono">
-									{relativeDate(data.date, data.time, true)}
-								</Tooltip.Content>
-							</Tooltip.Root>
+							<Skeleton class="w-[40px] h-[20px]" />
 						</Card.Title>
 						<Card.Description>
-							{data.location.join(", ")}
+							<Skeleton style="width: {Math.floor(Math.random()*250) + 40}px;" class="h-[20px]" />
 						</Card.Description>
 					</Card.Header>
 				</Card.Root>
-			</a>
-		{/each}
-		<InfiniteScroll window={true} threshold={100} on:loadMore={() => page++} />
-	</div>
-{:else if $history?.length === 0}
-	<p class="text-3xl font-semibold">אין התרעות.</p>
-{:else}
-	<div class="sm:w-[600px] w-[350px]">
-		<div class="flex flex-row items-center gap-7">
-			<Select.Root>
-				<Select.Trigger>
-					<Skeleton class="w-full h-full mx-2"/>
-				</Select.Trigger>
-			</Select.Root>
-			<Separator class="sm:w-80 w-28" />
+			{/each}
 		</div>
-		{#each new Array(12).fill("") as element}
-			<Card.Root class="my-4">
-				<Card.Header>
-					<Card.Title class="flex justify-between">
-						<div class="flex flex-row gap-2">
-							<Skeleton class="h-5 w-5 rounded-full" />
-							<Skeleton class="w-[200px] h-[20px]" />
-						</div>
-						<Skeleton class="w-[40px] h-[20px]" />
-					</Card.Title>
-					<Card.Description>
-						<Skeleton style="width: {Math.floor(Math.random()*250) + 40}px;" class="h-[20px]" />
-					</Card.Description>
-				</Card.Header>
-			</Card.Root>
-		{/each}
-	</div>
-{/if}
+	{/if}
+{/key}
