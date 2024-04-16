00389c94b2dddf3c20fb4ee2c0eca6c2bf28cab7
TheAnnoying <drork2018@gmail.com>
2024-04-16 23:48:47 +0300

    feat: added news to the ui

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -7,16 +7,19 @@
 	import nProgress from "nprogress";
 	import { browser } from "$app/environment";
 	import { navigating } from "$app/stores";
+	import { onMount } from "svelte";
 	import { routes } from "$lib/utils";
 	import { page } from "$app/stores";
 
 	import { connected, connectWebsocket } from "$lib/index.js";
-	import { Loader2, Info, FastForward } from "lucide-svelte";
-	import { fade } from "svelte/transition";
+	import { Loader2, Info, FastForward, X } from "lucide-svelte";
+	import { fade, slide } from "svelte/transition";
+	import { expoInOut } from "svelte/easing";
 	import { ModeWatcher } from "mode-watcher";
 
 	connectWebsocket();
 	nProgress.configure({
+		parent: "#navbar",
 		minimum: 0.4,
 		trickleSpeed: 200,
 		showSpinner: false,
@@ -24,7 +27,15 @@
 		speed: 300
 	});
 
-	let title;
+	let title, news = { text: null, display: false };
+	onMount(async() => {
+		const { text } = await import("../news.json");
+
+		if(text?.length > 0) {
+			news.text = text;
+			news.display = true;
+		}
+	});
 
 	$: {
 		if(browser) $navigating ? nProgress.start() : nProgress.done();
@@ -38,6 +49,13 @@
 
 <ModeWatcher />
 
+{#if news.display === true}
+	<div class="center row w-full p-3 bg-accent text-accent-foreground" transition:slide={{ duration: 400, easing: expoInOut }}>
+		<Button on:click={() => news.display = false} variant="primary" size="icon" class="absolute left-3 w-5 h-5"><X /></Button>
+		<p>{news.text}</p>
+	</div>
+{/if}
+
 <Button class="fixed -top-16 focus:top-4 left-4 z-50 transition-all gap-2" href="#main">
 	Skip Navigation
 	<FastForward class="w-3 h-3" />
@@ -50,7 +68,7 @@
 		<main id="main"><slot /></main>
 	</div>
 {:else}
-	<div class="fixed w-screen h-screen flex flex-row gap-5 justify-center items-center z-20" transition:fade={{ duration: 75 }}>
+	<div class="center row fixed w-screen h-screen gap-5 z-20" transition:fade={{ duration: 75 }}>
 		<Loader2 class="animate-spin h-11 w-11" />
 	</div>
 {/if}
