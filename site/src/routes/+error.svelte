b8929b501574a76798a3667c7dd38e4b5018bfbc
TheAnnoying <drork2018@gmail.com>
2024-04-17 21:00:17 +0300

    chore: made the error page a bit more professional

--- site/src/routes/+error.svelte
+++ site/src/routes/+error.svelte
@@ -1,13 +1,8 @@
 <script>
     import { page } from "$app/stores";
-    const type = [ "cat", "dog" ][Math.floor(Math.random()*2)];
+    import { Button } from "$lib/components/ui/button";
 </script>
-<img src="https://http.{type}/{$page.status}.jpg" alt="A {type} with the caption - 404 page not found" />
-<style>
-    img {
-        @apply fixed w-screen bottom-0 right-0;
-        @apply hue-rotate-180 invert dark:hue-rotate-0 dark:invert-0;
-        aspect-ratio: 16 / 9;
-        height: calc(100vh - 73px);
-    }
-</style>
+<div class="center col" style="height: calc(100vh - 83px)">
+    <h1 class="text-4xl font-semibold tracking-tight mb-2">{$page.status} - שגיאה</h1>
+    <Button href="/" variant="outline">חזרה לעמוד הבית</Button>
+</div>
