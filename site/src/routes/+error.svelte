ed734492a28ba519bda4fd6dfdc72176a06a194d
TheAnnoying <drork2018@gmail.com>
2024-07-06 14:10:23 +0300

    style: made margin bigger between text and home button

--- site/src/routes/+error.svelte
+++ site/src/routes/+error.svelte
@@ -3,6 +3,6 @@
     import { Button } from "$lib/components/ui/button";
 </script>
 <div class="center col" style="height: calc(100vh - 83px)">
-    <h1 class="text-4xl font-semibold tracking-tight mb-2">{$page.status} - שגיאה</h1>
+    <h1 class="text-4xl font-semibold tracking-tight mb-5">{$page.status} - שגיאה</h1>
     <Button href="/" variant="outline">חזרה לעמוד הבית</Button>
 </div>
