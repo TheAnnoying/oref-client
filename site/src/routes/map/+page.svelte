077f7c263cf4f6ec92ab7b43a94756b559a4e83d
TheAnnoying <me@theannoying.dev>
2025-06-17 20:23:50 +0300

    feat: added map page

---
+++ site/src/routes/map/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+    import "leaflet/dist/leaflet.css";
+	import locationList from "/src/locations.json";
+    import { onMount, tick } from "svelte";
+    import { alert } from "$lib/index.js";
+	import Alert from "$lib/components/ui/alert.svelte";
+    let locationsFound = [];
+
+    onMount(async() => {
+		const L = await import("leaflet");
+
+		const markerIcon = L.icon({
+			iconUrl: "/marker-icon.svg",
+			iconSize: [30, 42]
+		});
+
+        const map = L.map("map", { zoomControl: false }).setView([31.0461, 34.8516], 13);
+        map.setZoom(8);
+
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 13,
+            minZoom: 6,
+            attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+        }).addTo(map);
+
+		alert.subscribe(data => {
+			tick().then(() => {
+				const location = data.locations ?? [];
+
+				location.forEach(location => {
+					if(locationList[location]) locationsFound.push(location);
+				});
+
+                locationsFound.forEach(location => {
+                    const marker = L.marker([
+                        parseFloat(locationList[location].centerY),
+                        parseFloat(locationList[location].centerX)
+                    ], { icon: markerIcon }).addTo(map);
+
+                    marker.bindPopup(`
+                    <div style="text-align: center">
+                        <p style="margin: 0;">${location}</p>
+                        <p style="margin: 0;">זמן למיגון: ${locationList[location]["shelter_time"].text}</p>
+                    </div>
+                    `)
+                });
+			});
+		});
+	});
+</script>
+<Alert />
+<div id="map" class="w-full h-full absolute inset-0 bg-muted flex items-center justify-center !z-0">
+    {#if locationsFound.length < $alert?.locations?.length}
+        <p class="top-0 absolute transition-all text-md text-white bg-muted py-1 mt-1 px-3 rounded-md tracking-tight z-[400] flex items-center gap-1">
+            לא היה ניתן להציג על המפה את כל המיקומים
+        </p>
+    {/if}
+</div>
