8ffac50d00b3b48121246c5c755de87705374b0a
TheAnnoying <me@theannoying.dev>
2025-06-17 22:21:30 +0300

    fix: no longer crashes for some cities

--- site/src/lib/components/ui/alert-tags.svelte
+++ site/src/lib/components/ui/alert-tags.svelte
@@ -5,8 +5,8 @@
 </script>
 {#each $preferredLocations.flatMap(e => e[0]) as cities}
     {#each alertLocations.filter(e => cities.includes(e)) as city}
-        {#each $preferredLocations.find(e => e[0] == city)[1] as tag}
-            <Badge variant="outline" class="bg-yellow-100 dark:bg-yellow-500/20 text-yellow-700 dark:text-yellow-400 border-yellow-400 dark:border-yellow-500/30 !border-t-yellow-500/30">{tag}</Badge>
+        {#each $preferredLocations.find(e => e[0] == city)?.[1] ?? [] as tag}
+            <Badge variant="outline" class="bg-yellow-100 dark:bg-yellow-500/20 text-yellow-700 dark:text-yellow-400 border-yellow-400 dark:border-yellow-500/30 !border-t-yellow-500/30 dark:!border-b-yellow-500/30 !border-b-yellow-400">{tag}</Badge>
         {/each}
     {/each}
 {/each}
